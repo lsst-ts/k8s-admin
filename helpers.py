4257f269f5ae8c44a9dece2ea15a307ed229b180
Michael Reuter <mareuter@lsst.org>
2023-03-02 16:38:18 -0700

    Add deletion acknowledgement to hammer scripts.

--- helpers.py
+++ helpers.py
@@ -1,8 +1,11 @@
 import os
 import subprocess as sp
+import sys
 
 __all__ = [
+    "acknowledge_deletion",
     "API_MAPPING",
+    "GET_KUBECTL_CONTEXT",
     "GET_PODS_CMD",
     "GET_PODS_NAME_CMD",
     "run_cmd",
@@ -40,6 +43,8 @@ GET_PODS_CMD = (
 
 GET_PODS_NAME_CMD = "kubectl get pod -o=custom-columns=NAME:.metadata.name"
 
+GET_KUBECTL_CONTEXT = "kubectl config current-context"
+
 
 def run_cmd(command, as_lines=False):
     """Run a command via subprocess::run.
@@ -47,7 +52,7 @@ def run_cmd(command, as_lines=False):
     Parameters
     ----------
     command : str
-        The command to run. Shoud be space separated.
+        The command to run. Should be space separated.
     as_lines : bool, optional
         Return the output as a list instead of a string.
 
@@ -66,3 +71,15 @@ def run_cmd(command, as_lines=False):
         return decoded_output.split(os.linesep)
     else:
         return decoded_output[:-1]
+
+
+def acknowledge_deletion():
+    """Check the kubectl context before allowing deletion."""
+    output = run_cmd(GET_KUBECTL_CONTEXT)
+    print(f"{output} resources to be deleted. Are you sure?")
+    response = input("y to proceed, any other key to exit: ")
+    if response != "y":
+        print("Exiting")
+        sys.exit(255)
+    else:
+        print(f"Proceeding with {output} resource deletion.")
